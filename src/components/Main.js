9e82b9ac357c34db81598a7708cfd5d5dbbd7fd6
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-09-22 10:15:59 -0300

    adicionando função de excluir tarefas

--- src/components/Main.js
+++ src/components/Main.js
@@ -40,6 +40,21 @@ export default class Main extends Component{
 
   }
 
+  handleDelete = (e,index)=>{
+    const {tarefas} = this.state
+    const novasTarefas = [...tarefas]
+    novasTarefas.splice(index, 1)
+
+    this.setState({
+      tarefas:[...novasTarefas]
+    })
+  }
+
+  handleEdit = (e,index)=>{
+    console.log('Edit',index)
+
+  }
+
   render(){ 
     const {novaTarefa,tarefas} = this.state
 
@@ -62,13 +77,19 @@ export default class Main extends Component{
         </form>
 
         <ul className='tarefas'> 
-            {tarefas.map(tarefa => (
+            {tarefas.map((tarefa,index) => (
               <li key={tarefa}>
                 
                 {tarefa}
                 <span>
-                    <FaEdit className='edit'/>
-                    <FaWindowClose className='delete'/>
+                    <FaEdit 
+                      onClick={(e)=>this.handleEdit(e, index)}
+                      className='edit'
+                    />
+                    <FaWindowClose 
+                      onClick={(e)=>this.handleDelete(e, index)}
+                      className='delete'
+                    />
                 </span>
               </li>
             ))}
